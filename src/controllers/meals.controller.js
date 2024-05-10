20a495c14b8344717ba9ead573d9c44d013fa4b4
Esmee-Stam <el.stam@student.avans.nl>
2024-05-10 15:58:29 +0200

    Added meals.routes, meals.controller, meals.service

---
+++ src/controllers/meals.controller.js
@@ -0,0 +1,114 @@
+const mealsService = require('../services/meals.service')
+const logger = require('../util/logger')
+
+let mealsController = {
+    create: (req, res, next) => {
+        logger.info('create')
+
+        const meal = req.body
+        
+        mealsService.create(meal, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {}
+                })
+            }
+            if (success) {
+                res.status(201).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data
+                })
+            }
+        })
+    },
+
+    getAll: (req, res, next) => {
+        logger.info('getAll')
+        mealsService.getAll((error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {}
+                })
+            }
+            if (success) {
+                res.status(200).json({
+                    status: 200,
+                    message: success.message,
+                    data: success.data
+                })
+            }
+        })
+    },
+
+    getById: (req, res, next) => {
+        const mealId = req.params.mealId
+        logger.info('mealsController: getById', mealId)
+        mealsService.getById(mealId, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {}
+                })
+            }
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data
+                })
+            }
+        })
+    },
+
+    update: (req, res, next) => {
+        const mealId = req.params.mealId
+        const meal = req.body
+        logger.info('mealsController: update', mealId, meal)
+        mealsService.update(mealId, meal, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {}
+                })
+            }
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data
+                })
+            }
+        })
+    },
+
+    delete: (req, res, next) => {
+        const mealId = req.params.mealId
+        logger.info('mealsController: delete', mealId)
+        mealsService.delete(mealId, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {}
+                })
+            }
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data
+                })
+            }
+        })
+    }
+}
+
+module.exports = mealsController
+
