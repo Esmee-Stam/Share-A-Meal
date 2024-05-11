f0ea3d399c6300f3d3683908809d82a79dc190bf
Esmee-Stam <el.stam@student.avans.nl>
2024-05-11 15:11:28 +0200

    Added password in mysql-db

--- src/dao/mysql-db.js
+++ src/dao/mysql-db.js
@@ -7,6 +7,7 @@ const dbConfig = {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
 
     connectionLimit: 10,
     waitForConnections: true,
